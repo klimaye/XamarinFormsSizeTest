030fe963766891928f630bcb383dcd488de8d7e1
Kshitij Limaye <klimaye@gmail.com>
2015-09-13 11:42:24 -0500

    added xlabs.Forms dependencies to pcl, iOS and Droid projects

--- SizeTest.cs
+++ SizeTest.cs
@@ -4,6 +4,7 @@ using Xamarin.Forms;
 using System.Collections.Generic;
 using RestSharp.Portable;
 using System.Net.Http;
+using XLabs.Forms.Controls;
 
 namespace SizeTest
 {
@@ -12,6 +13,9 @@ namespace SizeTest
 		JsonSerializer serializer;
 		Dictionary<string,string> result;
 		RestClient restClient;
+		ImageButton imageButton;
+		HttpContent httpContent;
+		CheckBox checkbox;
 		public App ()
 		{
 			this.serializer = new JsonSerializer ();
@@ -19,7 +23,12 @@ namespace SizeTest
 							as Dictionary<string, string>;
 			this.restClient = new RestClient ();
 			var request = new RestRequest (new Uri ("http://google.com"), HttpMethod.Get);
-			var result = restClient.GetContent (request);
+			this.httpContent = restClient.GetContent (request);
+			this.imageButton = new ImageButton();
+			this.imageButton.Text = "Image Text";
+			this.imageButton.Source = "icon.png";
+			this.checkbox = new CheckBox ();
+			this.checkbox.Checked = true;
 			// The root page of your application
 			MainPage = new ContentPage {
 				Content = new StackLayout {
